5d07116fe4024fd087958c8c444172e68231535b
Rohin Madhav <rohinmadhavk7@gmail.com>
2025-10-25 12:42:56 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,24 @@ app.post(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://health-care-system-1-yzp8.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", healthCareRoutes);
 app.use("/api/users", userRoutes);
